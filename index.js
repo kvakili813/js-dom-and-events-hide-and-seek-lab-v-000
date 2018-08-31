3aa89f505d50fac482689bab3f87742ab49480a9
Kayvan Vakili <kvakili813@gmail.com>
2018-08-31 21:55:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return(document.querySelector(selector))
+}
+
+function nestedTarget() {
+  return(document.querySelector("#nested .target"))
+}
+
+function deepestChild() {
+  let list = document.getElementById("grand-node")
+  let listChild = list.children[0]
+
+  while (listChild) {
+    list = listChild
+    listChild = list.children[0]
+  }
+  
+  return list
+}
+
+function increaseRankBy(n) {
+  let list = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (i + n).toString()
+  }
+}
